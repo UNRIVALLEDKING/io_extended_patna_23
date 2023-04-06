800c020bac67ace2d53a3b78d9ab5943c929f8e8
UNRIVALLEDKING <adityakumarunrivalledking@gmail.com>
2023-04-06 14:27:54 +0530

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,25 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        googleRed: "#ea4335",
+        googleBlue: "#4285f4",
+        googleYellow: "#fabb04",
+        googleGreen: "0f9d57",
+      },
+      fontFamily: {
+        google: ["Plus Jakarta Sans", "sans-serif"],
+      },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
   },
   plugins: [],
 };
