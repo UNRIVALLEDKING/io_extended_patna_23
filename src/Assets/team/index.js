50212aab2079dc4a866dbddd878cdaecfd33ff7f
UNRIVALLEDKING <adityakumarunrivalledking@gmail.com>
2023-04-13 21:00:49 +0530

    public

--- src/Assets/team/index.js
+++ src/Assets/team/index.js
@@ -1,8 +1,9 @@
 import team1 from "./abc.jpg";
 import linkedinIcon from "./linkedin.png";
 import InstagramIcon from "./instagram.png";
-import TwitterIcon from "./twitter.png"; 
+import TwitterIcon from "./twitter.png";
 import Bg1 from "./b1.png";
-import Bg2 from "./b2.png"; 
+import Bg2 from "./b2.png";
+import TeamTri from "./bg.svg";
 
-export {team1,linkedinIcon,InstagramIcon,TwitterIcon,Bg1,Bg2};
+export { team1, linkedinIcon, InstagramIcon, TwitterIcon, Bg1, Bg2, TeamTri };
--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -2,6 +2,7 @@ import {
   Bg1,
   Bg2,
   InstagramIcon,
+  TeamTri,
   TwitterIcon,
   linkedinIcon,
   team1,
@@ -12,23 +13,27 @@ import { TeamData } from "./Teamdata";
 export default function Team() {
   return (
     <section id="team" className="relative">
-      Our Team
+      <h1 className="mt-10 mb-2 text-center capitalize text-2xl z-[999999]">
+        Our Team
+      </h1>
+      <hr className="w-1/5 mx-auto" />
       <div className="relative">
         <div className="w-1/2 absolute top-0 right-0 z-0 opacity-50">
           <Image src={Bg1} alt="io extended" />
         </div>
       </div>
-      <h1 className="mt-10 mb-2 text-center capitalize text-2xl">Our Team</h1>
-      <hr className="w-1/5 mx-auto" />
-      <div className="flex flex-row flex-wrap z-[999999] ">
+      <div className="flex flex-row mt-[150px] flex-wrap z-[999999]">
         {TeamData.map((item, id) => (
-          <div className="w-1/3 py-4 z-[999999]" key={id}>
-            <div className="flex flex-row items-center justify-center md:flex-col ">
+          <div className="w-full md:w-1/4 py-4 z-[999999]" key={id}>
+            <div className="flex flex-row items-start my-10 justify-center">
               <div
-                className={`w-1/2 bg-black ${item.topborder} border-t-2 p-2 rounded-t-full rounded-b-full pb-24 text-center`}
+                className={`w-2/3 md:w-1/2 bg-transparent text-center relative`}
               >
+                <div className="w-[105%] -translate-x-[2.5%] absolute bottom-6">
+                  <Image className="w-full" src={TeamTri} alt="io extended" />
+                </div>
                 <div
-                  className={`${item.color} p-2 rounded-t-full rounded-b-full pb-24`}
+                  className={`${item.color} -translate-y-[30%] p-2 rounded-t-full rounded-b-full pb-10 md:pb-24`}
                 >
                   <Image
                     src={team1}
@@ -44,44 +49,33 @@ export default function Team() {
                   <a href={item.linkedin} target="_blank" className="w-[50px]">
                     <Image
                       src={linkedinIcon}
-                      className="w-[50px] aspect-sqaure h-auto"
+                      className="w-[30px] md:w-[50px] aspect-sqaure h-auto"
                       alt="io extended"
                     />
                   </a>
                 </div>
                 <div className="">
-                  <Image
-                    src={InstagramIcon}
-                    className="w-[50px] aspect-sqaure h-auto"
-                    alt="io extended"
-                  />
+                  <a href={item.linkedin} target="_blank" className="w-[50px]">
+                    <Image
+                      src={InstagramIcon}
+                      className="w-[30px] md:w-[50px] aspect-sqaure h-auto"
+                      alt="io extended"
+                    />
+                  </a>
                 </div>
                 <div className="">
-                  <Image
-                    src={TwitterIcon}
-                    className="w-[50px] aspect-sqaure h-auto"
-                    alt="io extended"
-                  />
+                  <a href={item.linkedin} target="_blank" className="w-[50px]">
+                    <Image
+                      src={TwitterIcon}
+                      className="w-[30px] md:w-[50px] aspect-sqaure h-auto"
+                      alt="io extended"
+                    />
+                  </a>
                 </div>
               </div>
             </div>
           </div>
         ))}
-
-        {/* <div className="py-10">
-        <div className="rounded overflow-hidden shadow-lg max-w-sm">
-            <img src="https://cdn2.vectorstock.com/i/1000x1000/49/86/man-character-face-avatar-in-glasses-vector-17074986.jpg" 
-            alt="abc" 
-            className="rounded-full max-w-xs"
-            ></img>
-
-            <div className="px-6 py-4">
-                <div className="font-bold text-xl mb-2 "><b>abc</b></div>
-                <p1 className="text-gray-400">designation</p1>
-            </div>
-
-        </div>
-    </div> */}
       </div>
     </section>
   );
