fb2863f1d2ac7aa407afdb8aff8937e269b05063
UNRIVALLEDKING <adityakumarunrivalledking@gmail.com>
2023-05-15 22:42:04 +0530

    assets

---
+++ src/Assets/TeamMembers/index.js
@@ -0,0 +1,17 @@
+import Aditya from "./Aditya.jpg";
+import AnuragVerma from "./AnuragVerma.jpeg";
+import BarkhaAgarwal from "./BarkhaAgarwal.jpeg";
+import Shekhar from "./Shekhar.jpeg";
+import Hritik from "./Hritik.jpeg";
+import Priyanshu from "./Priyanshu.jpeg";
+import AdityaDesign from "./AdityaDesign.jpeg";
+
+export {
+  Aditya,
+  BarkhaAgarwal,
+  AnuragVerma,
+  Shekhar,
+  Hritik,
+  Priyanshu,
+  AdityaDesign,
+};
