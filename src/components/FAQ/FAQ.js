add607f3b07fe65a5bf5c36005d4625141da376a
UNRIVALLEDKING <adityakumarunrivalledking@gmail.com>
2023-05-16 17:10:24 +0530

    faq

--- src/components/FAQ/FAQ.js
+++ src/components/FAQ/FAQ.js
@@ -1,15 +1,14 @@
-
 import React from "react";
 import faqData from "./faqData.json";
 
 const FAQ = () => {
   return (
-    <section id="faq" className="bg-gray-100 py-8">
+    <section id="faq" className="py-8">
       <div className="container mx-auto">
         <h2 className="text-2xl font-bold mb-4">FAQ & Agenda</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
           {faqData.map((faq) => (
-            <div key={faq.id} className="bg-white p-4 shadow">
+            <div key={faq.id} className="p-4 shadow">
               <h3 className="text-lg font-medium mb-2">{faq.question}</h3>
               <p>{faq.answer}</p>
             </div>
@@ -21,4 +20,3 @@ const FAQ = () => {
 };
 
 export default FAQ;
-
