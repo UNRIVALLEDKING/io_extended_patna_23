490db41be7489a21504126f87bb19312b7c5e3e7
Jayam pandit <94307580+jayam30@users.noreply.github.com>
2023-05-16 23:09:29 +0530

    Update FAQ.js

--- src/components/FAQ/FAQ.js
+++ src/components/FAQ/FAQ.js
@@ -1,27 +1,17 @@
-import React from 'react';
-
-const faqData = [
-  {
-    question: 'Question 1',
-    answer: 'Answer 1',
-  },
-  {
-    question: 'Question 2',
-    answer: 'Answer 2',
-  },
-  // Add more question-answer pairs as needed
-];
+import faqData from './faqData.json';
 
 export default function FAQ() {
   return (
     <section id="faq" className="py-8">
-      <h2 className="text-2xl font-bold mb-4">Frequently Asked Questions</h2>
-      {faqData.map((faq, index) => (
-        <details key={index} className="mb-4">
-          <summary className="font-semibold">{faq.question}</summary>
-          <p className="mt-2">{faq.answer}</p>
-        </details>
-      ))}
+      <h2 className="text-2xl font-bold mb-4">FAQ &amp; Agenda</h2>
+      <div className="grid gap-4">
+        {faqData.map((faq, index) => (
+          <div key={index} className="border rounded p-4">
+            <h3 className="text-lg font-medium mb-2">{faq.question}</h3>
+            <p>{faq.answer}</p>
+          </div>
+        ))}
+      </div>
     </section>
   );
 }
