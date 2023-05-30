d878692a0099370f0c34935d30658d3303adfa86
UNRIVALLEDKING <adityakumarunrivalledking@gmail.com>
2023-05-30 11:57:27 +0530

    registration

--- src/components/Registrations/Registrations.js
+++ src/components/Registrations/Registrations.js
@@ -1,7 +1,30 @@
 export default function Registrations() {
   return (
     <section id="register" className="registration">
-      Registrations
+      <h1 className="mt-10 mb-2 text-center capitalize text-2xl z-[999999]">
+        Registration
+      </h1>
+      <hr className="w-1/2 md:w-1/5 mx-auto" />
+      <div
+        className="flex flex-row 
+      mt-[150px] mt-0
+       flex-wrap z-[999999] min-h-[50vh] justify-center items-center"
+      >
+        <p className="md:w-2/3 w-[90%] text-center text-2xl">
+          Attention tech enthusiasts and innovators! The countdown is on for the
+          highly anticipated Google I/O Extended Patna event, and we&apos;re
+          thrilled to announce that registration will be opening soon! Get ready
+          to secure your spot and be part of an extraordinary gathering of tech
+          minds. Whether you&apos;re a developer, designer, or simply fascinated
+          by the latest advancements, this is your chance to join us for a day
+          filled with knowledge-sharing, networking, and unforgettable
+          experiences. Stay tuned to our website for the official registration
+          launch, and be prepared to embark on a tech journey like no other.
+          Don&apos;t miss your opportunity to be part of this groundbreaking
+          event! 🚀 #GoogleIOExtendedPatna #RegistrationOpeningSoon
+          #TechJourneyAwaits
+        </p>
+      </div>
     </section>
   );
 }
